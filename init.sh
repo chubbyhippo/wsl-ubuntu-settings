07d6c1fcdb8dde9e86dcb0cc827defd3f80507e3
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-05-29 18:26:45 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -25,6 +25,3 @@ curl https://raw.githubusercontent.com/chubbyhippo/starship/main/install-bash.sh
 [ -f ~/.bashrc ] && curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/main/.bashrc >> ~/.bashrc || curl https://raw.githubusercontent.com/chubbyhippo/wsl-ubuntu/main/.bashrc -o ~/.bashrc
 # trivy
 curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.70.0
-# rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
