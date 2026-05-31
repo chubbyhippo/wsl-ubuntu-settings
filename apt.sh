23f7a63a5322fa4eebfdf0f51a004acc27dd75ca
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-05-31 14:56:08 +0700

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -2,13 +2,13 @@
 
 sudo apt install build-essential -y
 sudo apt install cmake -y
+sudo apt install curl -y
 sudo apt install fd-find -y
+sudo apt install file -y
+sudo apt install git -y
 sudo apt install leiningen -y
 sudo apt install markdown -y
 sudo apt install ripgrep -y
 sudo apt install rlwrap -y
 sudo apt install tmux -y
 sudo apt install unzip -y
-sudo apt install git -y
-sudo apt install curl -y
-sudo apt install file -y
