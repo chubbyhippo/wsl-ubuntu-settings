2769524b4bcbab8b85e64bf7771198fbd9f824b9
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-05-31 15:12:16 +0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> /home/vm/.bashrc
