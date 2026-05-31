0db30bd64ddda630ce7f7bb87ee71e9a5069c258
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-05-31 14:36:49 +0700

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -10,3 +10,6 @@ sudo apt install ripgrep -y
 sudo apt install rlwrap -y
 sudo apt install tmux -y
 sudo apt install unzip -y
+sudo apt install git -y
+sudo apt install curl -y
+sudo apt install file -y
