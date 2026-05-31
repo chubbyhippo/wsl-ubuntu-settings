53c9df9bfcf6fae1b86ac35b10ffc62d4a3ed587
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-05-31 15:11:45 +0700

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,4 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> /home/vm/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
