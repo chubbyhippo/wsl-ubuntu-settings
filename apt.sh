c89b6b5ec01716020cd9496d13384cf9410e98a5
chubbyhippo <mkmsacc@outlook.com>
2026-07-05 18:31:15 +0700

    Refactor `apt.sh` to streamline package installation with a single `apt install` command using a backslash-separated list.

--- apt.sh
+++ apt.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
 
-sudo apt install build-essential -y
-sudo apt install cmake -y
-sudo apt install curl -y
-sudo apt install fd-find -y
-sudo apt install file -y
-sudo apt install git -y
-sudo apt install guile-3.0 -y
-sudo apt install markdown -y
-sudo apt install procps -y
-sudo apt install ripgrep -y
-sudo apt install rlwrap -y
-sudo apt install tmux -y
-sudo apt install unzip -y
+sudo apt install -y \
+    build-essential \
+    cmake \
+    curl \
+    fd-find \
+    file \
+    git \
+    guile-3.0 \
+    markdown \
+    procps \
+    ripgrep \
+    rlwrap \
+    tmux \
+    unzip
